0dca596153d0f16b31f160d537c28a13aa9a9ab6
Peter Tränkle <peter.traenkle@liip.ch>
2026-02-07 22:05:10 +0100

    let disco use latest release and let it follow nixpkg

--- flake.nix
+++ flake.nix
@@ -3,7 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    disko.url = "github:nix-community/disko";
+    disko.url = "github:nix-community/disko/latest";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
     agenix.url = "github:ryantm/agenix";
   };
 
